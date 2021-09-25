80ce06934addead6c7d755af8c6ba2bd22859f21
maruware <me@maruware.com>
2021-09-25 21:17:18 +0900

    Set up for packing

--- electron/main.js
+++ electron/main.js
@@ -9,7 +9,7 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 1024, height: 768 });
 
   // and load the index.html of the app.
   const startUrl =
@@ -22,7 +22,7 @@ function createWindow() {
   mainWindow.loadURL(startUrl);
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on("closed", function() {
