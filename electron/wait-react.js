8ff9128c37564304ed760b1ac721e6715727ba3e
maruware <me@maruware.com>
2021-09-28 22:50:57 +0900

    Fix electron scripts

--- electron/wait-react.js
+++ electron/wait-react.js
@@ -1,27 +1,26 @@
-const net = require("net");
-const port = process.env.PORT ? process.env.PORT - 100 : 3000;
+/* eslint-disable @typescript-eslint/no-var-requires */
+const net = require('net')
+const child_process = require('child_process')
+const port = process.env.PORT ? process.env.PORT - 100 : 3000
 
-process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+process.env.ELECTRON_START_URL = `http://localhost:${port}`
 
-const client = new net.Socket();
+const client = new net.Socket()
 
-let startedElectron = false;
+let startedElectron = false
 const tryConnection = () =>
-  client.connect(
-    { port: port },
-    () => {
-      client.end();
-      if (!startedElectron) {
-        console.log("starting electron");
-        startedElectron = true;
-        const exec = require("child_process").exec;
-        exec("npm run electron");
-      }
+  client.connect({ port: port }, () => {
+    client.end()
+    if (!startedElectron) {
+      console.log('starting electron')
+      startedElectron = true
+      const exec = child_process.exec
+      exec('npm run electron')
     }
-  );
+  })
 
-tryConnection();
+tryConnection()
 
-client.on("error", (error) => {
-  setTimeout(tryConnection, 1000);
-});
+client.on('error', (error) => {
+  setTimeout(tryConnection, 1000)
+})
