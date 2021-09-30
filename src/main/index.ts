49937441ead77f7ff614f1be8ef28e01d88e19a8
maruware <me@maruware.com>
2021-09-30 23:46:23 +0900

    Refactor

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,49 +8,23 @@ const extPath =
     ? '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.13.2_0'
     : '/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.13.2_0'
 
-/**
- * BrowserWindowインスタンスを作成する関数
- */
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      /**
-       * BrowserWindowインスタンス（レンダラープロセス）では
-       * Node.jsの機能を無効化する（electron@8以降でデフォルト）
-       */
       nodeIntegration: false,
-      /**
-       * メインプロセスとレンダラープロセスとの間で
-       * コンテキストを共有しない (electron@12以降でデフォルト)
-       */
       contextIsolation: true,
-      /**
-       * Preloadスクリプト
-       * webpack.config.js で 'node.__dirname: false' を
-       * 指定していればパスを取得できる
-       */
       preload: path.join(__dirname, 'preload.js'),
     },
   })
 
-  // 開発時にはデベロッパーツールを開く
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools({ mode: 'detach' })
   }
 
-  // レンダラープロセスをロード
   mainWindow.loadFile('dist/index.html')
 }
 
-/**
- * アプリを起動する準備が完了したら BrowserWindow インスタンスを作成し、
- * レンダラープロセス（index.htmlとそこから呼ばれるスクリプト）を
- * ロードする
- */
 app.whenReady().then(async () => {
-  /**
-   * 開発時には React Developer Tools をロードする
-   */
   if (process.env.NODE_ENV === 'development') {
     await session.defaultSession
       .loadExtension(path.join(os.homedir(), extPath), {
@@ -60,11 +34,9 @@ app.whenReady().then(async () => {
       .catch((err) => console.log(err))
   }
 
-  // BrowserWindow インスタンスを作成
   createWindow()
 })
 
-// すべてのウィンドウが閉じられたらアプリを終了する
 app.once('window-all-closed', () => app.quit())
 
 // ipc
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,13 @@
 import path from 'path'
 
-/** エディタで補完を効かせるために型定義をインポート */
 import { Configuration } from 'webpack'
 
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 const isDev = process.env.NODE_ENV === 'development'
 
-/** 共通設定 */
 const base: Configuration = {
   mode: isDev ? 'development' : 'production',
-  // メインプロセスで __dirname でパスを取得できるようにする
   node: {
     __dirname: false,
     __filename: false,
@@ -20,60 +16,29 @@ const base: Configuration = {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
   },
   output: {
-    // バンドルファイルの出力先（ここではプロジェクト直下の 'dist' ディレクトリ）
     path: path.resolve(__dirname, 'dist'),
-    // webpack@5.x + electron では必須の設定
     publicPath: './',
     filename: '[name].js',
-    // 画像などのアセットは 'assets' フォルダへ配置する
     assetModuleFilename: 'assets/[name][ext]',
   },
   module: {
     rules: [
       {
-        /**
-         * 拡張子 '.ts' または '.tsx' （正規表現）のファイルを 'ts-loader' で処理
-         * node_modules ディレクトリは除外する
-         */
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: 'ts-loader',
       },
       {
-        /** 拡張子 '.css' （正規表現）のファイル */
-        test: /\.css$/,
-        /** use 配列に指定したローダーは *最後尾から* 順に適用される */
-        use: [
-          /* セキュリティ対策のため（後述）style-loader は使用しない **/
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { sourceMap: isDev },
-          },
-        ],
-      },
-      {
-        /** 画像やフォントなどのアセット類 */
         test: /\.(bmp|ico|gif|jpe?g|png|svg|ttf|eot|woff?2?)$/,
-        /** アセット類も同様に asset/inline は使用しない */
-        /** なお、webpack@5.x では file-loader or url-loader は不要になった */
         type: 'asset/resource',
       },
     ],
   },
-  /**
-   * developmentモードではソースマップを付ける
-   *
-   * レンダラープロセスでは development モード時に
-   * ソースマップがないと electron のデベロッパーコンソールに
-   * 'Uncaught EvalError' が表示されてしまうことに注意
-   */
   devtool: isDev ? 'inline-source-map' : false,
 }
 
 // for main
 const main: Configuration = {
-  // 共通設定の読み込み
   ...base,
   target: 'electron-main',
   entry: {
@@ -93,16 +58,12 @@ const preload: Configuration = {
 // for renderer
 const renderer: Configuration = {
   ...base,
-  // セキュリティ対策として 'electron-renderer' ターゲットは使用しない
+  // no use 'electron-renderer' for security
   target: 'web',
   entry: {
     renderer: './src/renderer/index.tsx',
   },
   plugins: [
-    /**
-     * バンドルしたJSファイルを <script></script> タグとして差し込んだ
-     * HTMLファイルを出力するプラグイン
-     */
     new HtmlWebpackPlugin({
       template: './src/index.html',
       minify: !isDev,
@@ -110,7 +71,6 @@ const renderer: Configuration = {
       filename: 'index.html',
       scriptLoading: 'blocking',
     }),
-    new MiniCssExtractPlugin(),
   ],
 }
 
