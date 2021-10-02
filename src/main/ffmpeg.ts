0883f99f42e45deff90bb7aa713a2c1e2f97fbbe
maruware <me@maruware.com>
2021-10-02 14:40:46 +0900

    Fix PATH on darwin

--- src/main/ffmpeg.ts
+++ src/main/ffmpeg.ts
@@ -1,28 +1,44 @@
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
 import stringArgv from 'string-argv'
+import os from 'os'
 
-let process: ChildProcessWithoutNullStreams | null = null
+let proc: ChildProcessWithoutNullStreams | null = null
+
+const pathEnv = () => {
+  if (os.platform() === 'darwin') {
+    if (process.env.PATH) {
+      return `${process.env.PATH}:/usr/local/bin`
+    } else {
+      return '/usr/local/bin'
+    }
+  }
+  return process.env.PATH
+}
 
 export const spawnFfmpeg = (
   cmd: string,
   onOutput: (o: string) => void,
   onClose: (code: number | null) => void
 ): void => {
-  if (process) {
-    process.kill('SIGINT')
-    process = null
+  if (proc) {
+    proc.kill('SIGINT')
+    proc = null
   }
   const [file, ...args] = stringArgv(cmd)
-  process = spawn(file, args)
-  process.stderr.on('data', (data) => {
+  proc = spawn(file, args, {
+    env: {
+      PATH: pathEnv(),
+    },
+  })
+  proc.stderr.on('data', (data) => {
     onOutput(data.toString())
   })
-  process.on('close', (code) => {
+  proc.on('close', (code) => {
     onClose(code)
   })
 }
 
 export const killFfmpeg = (): void => {
-  if (!process) return
-  process.kill('SIGINT')
+  if (!proc) return
+  proc.kill('SIGINT')
 }
